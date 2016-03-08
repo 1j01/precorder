b4a3803e89a57451d5ee9a44f9c9ebc67425f0a3
Isaiah Odhner <isaiahodhner@gmail.com>
2016-03-08 18:39:38 -0500

    Clean up

--- cli.js
+++ cli.js
@@ -1,27 +1,3 @@
 #!/usr/bin/env node
-
-var arg = process.argv[2];
-
-var help = "\n\
-Usage: precord [time]\n\
-\n\
-Examples:\n\
-\n\
-  $ precord 5min\n\
-  $ precord 10s\n\
-\n\
-Options:\n\
-\n\
-  -h, --help     output usage information\n\
-  -V, --version  output the version number\n\
-";
-
-if(!arg || arg.match(/^(--help|-h|help)$/)){
-	console.log(help);
-}else if(arg.match(/^(-v|-V|--version)$/)){
-	console.log(require("./package").version);
-}else{
-	console.log(arg, "huh? how about just the whole thing?");
-	require("coffee-script/register");
-	require("./precord.coffee");
-}
+require("coffee-script/register");
+require("./src/cli.coffee");
---
+++ src/cli.coffee
@@ -0,0 +1,24 @@
+
+arg = process.argv[2]
+
+help = """
+	Usage: precord [time]
+
+	Examples:
+
+	  $ precord 5min
+	  $ precord 10s
+
+	Options:
+
+	  -h, --help     output usage information
+	  -V, --version  output the version number
+"""
+
+if not arg or arg in ["--help", "-h"]
+	console.log help
+else if arg in ["--version", "-V", "-v"]
+	console.log require("./package").version
+else
+	console.log "#{arg}, huh? how about just the whole thing?"
+	require("./precord.coffee")
